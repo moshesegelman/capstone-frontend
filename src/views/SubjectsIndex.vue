ce34f0e4def773274e76c64c86e0c3d6acb3ba5a
moshesegelman <moshesegelman41@gmail.com>
2020-07-21 13:17:25 -0400

    finishes subject index page

--- src/views/SubjectsIndex.vue
+++ src/views/SubjectsIndex.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="subject-index">
     <h1>{{ message }}</h1>
+    <div v-for="subject in subjects">
+      <h1>Subejct: {{ subject.name }}</h1>
+      <img :src="subject.image_url" >
+    </div>
   </div>
 </template>
 
 <style>
+img {
+  width: 40em;
+  height: 15em;
+}
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Subject Index"
+      message: "Welcome to Subject Index",
+      subjects: []
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/subjects").then(response => {
+      this.subjects = response.data;
+      console.log(this.subjects);
+    });
+  },
   methods: {}
 };
 </script>
